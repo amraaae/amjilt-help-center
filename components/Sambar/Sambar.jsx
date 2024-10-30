8b07f0da176ff69c440d4e56af105ed3be1fd35a
amraaae <AMRAAAE6@GMAIL.COM>
2024-10-30 18:38:12 +0800

    a

--- components/Sambar/Sambar.jsx
+++ components/Sambar/Sambar.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function Sambar() {
   return (
-    <div>
-      <h1>Sambar</h1>
+    <div className="w-full px-4">
+      <h1 className="text-2xl font-bold">Самбар</h1>
+      <p className="text-xl">
+        Дэлгэцийн зүүн дээд талд самбар хэсэг байрлах ба энэ хуудсаас та
+        статистик мэдээлэлүүдтэй дэлгэрэнгүй танилцах боломжтой болно.
+      </p>
+      <ul className="max-w-md space-y-1 text-black list-disc list-inside">
+        <li>Нийт төгссөн ангийн тоо</li>
+        <li>Идэвхитэй анги</li>
+        <li>Сургалтууд</li>
+        <li>Контентийн тоо</li>
+      </ul>
     </div>
   );
 }
