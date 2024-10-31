2d259854b4b01333b9828820a2a71d3ff8fe1028
amraaae <AMRAAAE6@GMAIL.COM>
2024-10-31 11:09:40 +0800

    s

--- components/Sambar/Sambar.jsx
+++ components/Sambar/Sambar.jsx
@@ -1,20 +1,47 @@
+import Image from "next/image";
 import React from "react";
-
 
 function Sambar() {
   return (
-    <div className="w-full px-4">
+    <div className="w-full px-20">
       <h1 className="text-2xl font-bold">Самбар</h1>
-      <p className="text-xl">
+      <p className="text-lg py-5">
         Дэлгэцийн зүүн дээд талд самбар хэсэг байрлах ба энэ хуудсаас та
         статистик мэдээлэлүүдтэй дэлгэрэнгүй танилцах боломжтой болно.
       </p>
-      <ul className="max-w-md space-y-1 text-black list-disc list-inside">
-        <li>Нийт төгссөн ангийн тоо</li>
-        <li>Идэвхитэй анги</li>
-        <li>Сургалтууд</li>
-        <li>Контентийн тоо</li>
-      </ul>
+      <div>
+        <ul className="max-w-md space-y-1 text-black list-disc list-inside">
+          <li>Нийт төгссөн ангийн тоо</li>
+          <li>Идэвхитэй анги</li>
+          <li>Сургалтууд</li>
+          <li>Нийт сурагчидийн тоо</li>
+          <li>Багш ажилчидийн тоо</li>
+          <li>Сургуулийн зарлалууд</li>
+          <li>Төлбөрийн мэдээлэл /Нийт төлбөр, Төлөгдсөн, Дутуу/</li>
+        </ul>
+      </div>
+
+      <div className="py-5">
+        <Image
+          src="/Picture1.jpg"
+          width={1000}
+          height={1000}
+          alt="Picture1"
+        ></Image>
+      </div>
+      <div>
+        <p className="text-lg py-5">
+          Самбар хэсгийн дэлгэцийн баруун талд мэдээллүүд харагдана.
+        </p>
+        <ul className="max-w-md space-y-1 text-black list-disc list-inside">
+          <li>Хувийн мэдээлэл</li>
+          <li>Нууцлал тохиргоо</li>
+          <li>Гарах</li>
+          <li>Зарлал үүсгэх /Холбоос/</li>
+          <li>Хичээллийн ирц оруулах /Холбоос/</li>
+          <li>Контент оруулах /Холбоос/</li>
+        </ul>
+      </div>
     </div>
   );
 }
