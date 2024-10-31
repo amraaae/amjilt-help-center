7539a4972a78cc6a6f424181b24a321af4ae4e58
amraaae <AMRAAAE6@GMAIL.COM>
2024-10-31 12:26:27 +0800

    update

--- components/Surgalttow/Surgalt.jsx
+++ components/Surgalttow/Surgalt.jsx
@@ -7,7 +7,7 @@ function Surgalttow() {
       <h1 className="text-2xl font-bold">Сургалтын төв</h1>
       <p className="text-lg py-5">
         Дэлгэцийн зүүн дээд талд сургалтын төв хэсэгт дарж дараах мэдээллүүдийг
-        харах боломжтой..{" "}
+        харах боломжтой.
       </p>
 
       <div className="flex flex-col md:mb-auto flex-1">
@@ -24,7 +24,7 @@ function Surgalttow() {
       <p className="text-lg py-5">
         Дээрх хэсгүүдээс өөрийн мэдээлэл нэмэх шаардлагатай хэсэгээ дарж
         дэлгэцийн зүүн дээд хэсэгт байх ногоон өнгийн нэмэх гэсэн дээр дарж
-        мэдээллээ оруулж болно. /ТМС/{" "}
+        мэдээллээ оруулж болно. /ТМС/
       </p>
 
       <div className="py-4">
@@ -34,27 +34,102 @@ function Surgalttow() {
           width={1000}
           height={1000}
           alt="Picture1"
-        ></Image>
+        />
       </div>
 
       <p className="text-lg py-5">
         Хэрэв админ шинэ сургалт нэмэх бол эхлээд тухайн сургалтыг хөтөлбөр
         хичээлүүд хэсэгт шинээр оруулах шаардлагатай болно. Шинэ хөтөлбөр хичээл
-        нэмэх үедээ дараах зааврын дагуу хийнэ.{" "}
+        нэмэх үедээ дараах зааврын дагуу хийнэ.
       </p>
 
-      <div class="flex flex-col space-y-6">
-        <div class="flex space-x-4 items-center">
-          <div class="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
-            <span class="text-white text-sm font-bold">1</span>
+      <div className="flex flex-col space-y-6">
+        <div className="flex space-x-4 items-center">
+          <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+            <span className="text-white text-sm font-bold">1</span>
           </div>
-          <h2 class="text-lg font-medium text-gray-800">Сургалтын төв хуудсын хөтөлбөр хичээлүүд хэсгийг сонгоно.</h2>
+          <h2 className="text-lg font-medium text-gray-800">
+            Сургалтын төв хуудсын хөтөлбөр хичээлүүд хэсгийг сонгоно.
+          </h2>
         </div>
-        <div class="flex space-x-4 items-center">
-          <div class="rounded-full h-6 w-6 bg-gray-300 flex items-center justify-center">
-            <span class="text-white text-sm font-bold">2</span>
+        <div className="flex space-x-4 items-center">
+          <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+            <span className="text-white text-sm font-bold">2</span>
           </div>
-          <h2 class="text-lg font-medium text-gray-800">Дэлгэцийн зүүн дээд хэсэгт байх ногоон өнгийн нэмэх гэсэн дээр дарна.</h2>
+          <h2 className="text-lg font-medium text-gray-800">
+            Дэлгэцийн зүүн дээд хэсэгт байх ногоон өнгийн нэмэх гэсэн дээр
+            дарна.
+          </h2>
+        </div>
+      </div>
+
+      <div className="py-4">
+        <Image
+          className="w-full h-[500px]"
+          src="/Picture4.jpg"
+          width={1000}
+          height={1000}
+          alt="Picture2"
+        />
+      </div>
+
+      <p className="text-lg py-5">
+        Нэмэх хэсэгт дарсны дараа дэлгэц дараах байдлаар харагдана. /ТМС/
+      </p>
+
+      <div className="hero ">
+        <div className="hero-content w-full flex-col lg:flex-row">
+          <Image
+            src="/Picture5.jpg"
+            width={1000}
+            height={1000}
+            alt="Picture5"
+            className="max-w-md"
+          />
+          <div className="px-10 w-full">
+            <div className="flex flex-col space-y-6">
+              <div className="flex space-x-4 items-center">
+                <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+                  <span className="text-white text-sm font-bold">1</span>
+                </div>
+                <h2 className="text-lg font-medium text-gray-800">
+                  Хөтөлбөр хичээлийн нэрийг оруулана.
+                </h2>
+              </div>
+              <div className="flex space-x-4 items-center">
+                <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+                  <span className="text-white text-sm font-bold">2</span>
+                </div>
+                <h2 className="text-lg font-medium text-gray-800">
+                  Хичээллийн нийт судлах цагыг оруулана.
+                </h2>
+              </div>
+              <div className="flex space-x-4 items-center">
+                <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+                  <span className="text-white text-sm font-bold">3</span>
+                </div>
+                <h2 className="text-lg font-medium text-gray-800">
+                  Хичээллийн хөтөлбрийн танилцуулгаа оруулана.
+                </h2>
+              </div>
+              <div className="flex space-x-4 items-center">
+                <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+                  <span className="text-white text-sm font-bold">4</span>
+                </div>
+                <h2 className="text-lg font-medium text-gray-800">
+                  Хичээл заах багшийг сонгоно.
+                </h2>
+              </div>
+              <div className="flex space-x-4 items-center">
+                <div className="rounded-full h-6 w-6 bg-blue-500 flex items-center justify-center">
+                  <span className="text-white text-sm font-bold">5</span>
+                </div>
+                <h2 className="text-lg font-medium text-gray-800">
+                  Оруулсан мэдээлэлээ шалгаад хадгалах хэсэгт дарна.
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
